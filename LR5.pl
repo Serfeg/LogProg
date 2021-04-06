6312e6b14714c0629bffccfad79e83fab4939362
unknown <70938016+Serfeg@users.noreply.github.com>
2021-04-06 16:36:01 +0700

    Доделанная 5 лабораторная работа

--- LR5.pl
+++ LR5.pl
@@ -18,4 +18,7 @@ hexadecimalPredicat(X):-
 	X1 is X,
 	binaryPredicat(X1).
 
+buildtriangle(X1,Y1,X2,Y2,X3,Y3):-((X1 = X2,Y1 = Y2);(X1 = X3,Y1=Y3);(X2 = X3,Y2=Y3)), write('Нельзя'),!.
+buildtriangle(X1,Y1,X2,Y2,X3,Y3):- (Y3-Y1)*(X2-X1) \= (X3-X1)*(Y2-Y1), write('Можно').
+
 
